5f1f258b3135a8b1b4081e12860c09e1d0b60424
Car456 <gcarlosandree@yahoo.es>
2016-08-03 23:35:58 -0600

    Practica1

---
+++ Practica1/app/src/main/java/com/ejemplo/carlosgamboa/practica1/Practica1.java
@@ -0,0 +1,13 @@
+package com.ejemplo.carlosgamboa.practica1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Practica1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practica1);
+    }
+}
